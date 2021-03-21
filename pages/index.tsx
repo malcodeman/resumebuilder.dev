2484d2d045c4b56cbfb726a45cd733b38d2c61bb
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-03-21 18:10:11 +0100

    Adds next.js

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CV-builder</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>CV builder</div>
+    </div>
+  );
+}
+
+export default Home;
