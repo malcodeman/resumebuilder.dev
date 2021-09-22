2f92378c446ba0d2a30270430f027d911c83acc1
malcodeman <26983729+malcodeman@users.noreply.github.com>
2021-09-22 14:54:53 +0200

    Adds Fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from "next/app";
+import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import * as Fathom from "fathom-client";
 import "emoji-mart/css/emoji-mart.css";
 
 function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    Fathom.load(process.env.FATHOM_SITE_ID, {
+      includedDomains: ["resumebuilder.dev", "wwww.resumebuilder.dev"],
+    });
+  }, []);
   return (
     <ChakraProvider>
       <Component {...pageProps} />
