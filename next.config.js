a8f341ee1a673e600f5ee3b8464e321cc146a3db
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-08-26 20:42:10 +0200

    Updates pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
-  },
-  productionBrowserSourceMaps: true,
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
+
+module.exports = withPWA();
