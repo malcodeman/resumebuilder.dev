516ae8207e15dc9d11b6d60904a0c221402570c4
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-09-15 15:10:29 +0200

    Updates cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    video: false,
+    screenshotOnRunFailure: false,
     baseUrl: "http://localhost:3000",
   },
 });
