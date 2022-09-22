da791a8c8e76fa24c4a9cb3aaeac81f1dc4b4f7f
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-09-22 11:27:00 +0200

    Enables fathom only on production env.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { load } from "fathom-client";
 import "emoji-mart/css/emoji-mart.css";
 
-import { FATHOM_SITE_ID } from "../lib/constants";
+import { FATHOM_SITE_ID, IS_PROD } from "../lib/constants";
 
 import ErrorBoundary from "../components/misc/ErrorBoundary";
 
@@ -26,10 +26,12 @@ const THEME = extendTheme({
 
 function App({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
-    load(FATHOM_SITE_ID, {
-      url: "https://warbler.resumebuilder.dev/script.js",
-      includedDomains: ["resumebuilder.dev", "www.resumebuilder.dev"],
-    });
+    if (IS_PROD) {
+      load(FATHOM_SITE_ID, {
+        url: "https://warbler.resumebuilder.dev/script.js",
+        includedDomains: ["resumebuilder.dev", "www.resumebuilder.dev"],
+      });
+    }
   }, []);
   return (
     <ChakraProvider theme={THEME}>
