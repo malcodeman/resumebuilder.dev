461bd7809dec1a23025e4228c6f3f455a5bdf942
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-10-02 14:13:01 +0200

    Translates downloads page

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Box,
   Grid,
@@ -11,32 +12,39 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import Head from "next/head";
-import { map, toLower } from "ramda";
+import { equals, map, toLower } from "ramda";
 import AppleIcon from "simple-icons/icons/apple";
 import MicrosoftIcon from "simple-icons/icons/microsoft";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 import Layout from "../components/Layout";
 import SimpleIcon from "../components/misc/SimpleIcon";
 
 function Downloads() {
+  const { t } = useTranslation();
   const { colorMode } = useColorMode();
-  const DOWNLOADS = [
-    {
-      backgroundImage:
-        colorMode === "dark" ? "/macos_dark.jpg" : "/macos_light.jpg",
-      icon: <SimpleIcon color="#fff" size={48} path={AppleIcon.path} />,
-      title: "MacOS",
-      text: "Download for MacOS",
-      isDisabled: true,
-    },
-    {
-      backgroundImage: "/windows.jpg",
-      icon: <SimpleIcon color="#fff" size={48} path={MicrosoftIcon.path} />,
-      title: "Windows",
-      text: "Download for Windows",
-      isDisabled: true,
-    },
-  ];
+  const downloads = React.useMemo(
+    () => [
+      {
+        backgroundImage: equals(colorMode, "dark")
+          ? "/macos_dark.jpg"
+          : "/macos_light.jpg",
+        icon: <SimpleIcon color="#fff" size={48} path={AppleIcon.path} />,
+        title: "MacOS",
+        textTransKey: "download_for_macos",
+        isDisabled: true,
+      },
+      {
+        backgroundImage: "/windows.jpg",
+        icon: <SimpleIcon color="#fff" size={48} path={MicrosoftIcon.path} />,
+        title: "Windows",
+        textTransKey: "download_for_windows",
+        isDisabled: true,
+      },
+    ],
+    [colorMode]
+  );
   return (
     <>
       <Head>
@@ -45,14 +53,14 @@ function Downloads() {
       <Layout>
         <Center mb="4">
           <Heading overflowWrap="anywhere">
-            resumebuilder.dev for Mac & Windows
+            {t("resume_builder_for_mac_and_windows")}
           </Heading>
         </Center>
         <Container maxW="container.md" paddingInline="0">
           <Grid templateColumns={["1fr", "1fr 1fr"]} gap="8">
             {map(
               (item) => (
-                <Tooltip key={item.title} label="Coming Soon">
+                <Tooltip key={item.title} label={t("coming_soon")}>
                   <Flex flexDirection="column">
                     <Box
                       padding="4"
@@ -73,18 +81,26 @@ function Downloads() {
                       data-cy={`downloads-${toLower(item.title)}`}
                       isDisabled={item.isDisabled}
                     >
-                      {item.text}
+                      {t(item.textTransKey)}
                     </Button>
                   </Flex>
                 </Tooltip>
               ),
-              DOWNLOADS
+              downloads
             )}
           </Grid>
         </Container>
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
 
 export default Downloads;
