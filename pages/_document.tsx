e8288cf04e5e888518ff4f0bd6938c904ff4ec58
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-10-29 20:55:24 +0200

    Updates social media related meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,14 +12,33 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <meta charSet="utf-8" />
           <meta
+            property="og:title"
+            content="Free Resume Builder for Developers | resumebuilder.dev"
+          />
+          <meta
             name="og:description"
-            content="resumebuilder.dev - free resume builder for developers"
+            content="The fastest way to build your tech resume."
           />
           <meta
             name="description"
-            content="resumebuilder.dev - free resume builder for developers"
+            content="The fastest way to build your tech resume."
           />
           <meta property="og:image" content="opengraph/index.png"></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta property="og:site_name" content="https://resumebuilder.dev" />
+          <meta property="og:type" content="website" />
+          <meta property="og:determiner" content="a" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:url" content="https://resumebuilder.dev" />
+          <meta
+            name="twitter:title"
+            content="Free Resume Builder for Developers | resumebuilder.dev"
+          />
+          <meta
+            name="twitter:description"
+            content="The fastest way to build your tech resume"
+          />
+          <meta name="twitter:image" content="opengraph/index.png" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
