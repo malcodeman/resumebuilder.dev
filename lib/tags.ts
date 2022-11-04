ff2216f56642db5ec59e6295ec49184d185c4fb8
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-11-04 21:14:20 +0100

    Adds suggested tags

---
+++ lib/tags.ts
@@ -0,0 +1,88 @@
+import { nanoid } from "nanoid";
+
+const SKILLS = [
+  {
+    id: nanoid(),
+    value: "React",
+  },
+  {
+    id: nanoid(),
+    value: "Next.js",
+  },
+  {
+    id: nanoid(),
+    value: "Node.js",
+  },
+  {
+    id: nanoid(),
+    value: "Deno",
+  },
+  {
+    id: nanoid(),
+    value: "TypeScript",
+  },
+  {
+    id: nanoid(),
+    value: "JavaScript",
+  },
+  {
+    id: nanoid(),
+    value: "Python",
+  },
+  {
+    id: nanoid(),
+    value: "Rust",
+  },
+  {
+    id: nanoid(),
+    value: "Go",
+  },
+  {
+    id: nanoid(),
+    value: "AWS",
+  },
+  {
+    id: nanoid(),
+    value: "Azure",
+  },
+  {
+    id: nanoid(),
+    value: "Cypress",
+  },
+  {
+    id: nanoid(),
+    value: "Playwright ",
+  },
+  {
+    id: nanoid(),
+    value: "Storybook",
+  },
+];
+const HOBBIES = [
+  {
+    id: nanoid(),
+    value: "Cooking",
+  },
+  {
+    id: nanoid(),
+    value: "Reading",
+  },
+  {
+    id: nanoid(),
+    value: "Gaming",
+  },
+  {
+    id: nanoid(),
+    value: "Traveling",
+  },
+  {
+    id: nanoid(),
+    value: "Gardening",
+  },
+];
+const EXPORTS = {
+  SKILLS,
+  HOBBIES,
+};
+
+export default EXPORTS;
