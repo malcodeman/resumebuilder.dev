8f74de7e9a012db6b958940cf44ca705120a179a
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-08 17:49:21 +0100

    Fixes svg related console warnings

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -14,10 +14,10 @@ function Logo(props: props) {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             version="1"
             viewBox="0 0 48 48"
-            enable-background="new 0 0 48 48"
+            enableBackground="new 0 0 48 48"
             height="24"
             width="24"
             xmlns="http://www.w3.org/2000/svg"
