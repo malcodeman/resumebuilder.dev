0ccadc2fe7ce3056504293b896676277fe91c609
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-08 17:33:17 +0100

    Improves hero cta button wording

--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -23,12 +23,12 @@ describe("Landing page", () => {
       });
     cy.url().should("eq", `${Cypress.config().baseUrl}/resumes`);
   });
-  it("Build resume button", () => {
+  it("Build for free top button", () => {
     cy.intercept({
       method: "GET",
       url: "**/resumes/**",
     }).as("getResume");
-    cy.get("[data-cy=build-resume-button]")
+    cy.get("[data-cy=build-for-free-top-button]")
       .click()
       .should(() => {
         expect(JSON.parse(localStorage.getItem("resumes"))).to.be.a("array");
@@ -58,12 +58,12 @@ describe("Landing page", () => {
     cy.get("[data-cy=accordion-button]").click({ multiple: true });
     cy.get("[data-cy=accordion-panel]").should("have.length", 6);
   });
-  it("Build for free button", () => {
+  it("Build for free bottom button", () => {
     cy.intercept({
       method: "GET",
       url: "**/resumes/**",
     }).as("getResume");
-    cy.get("[data-cy=build-for-free-button]")
+    cy.get("[data-cy=build-for-free-bottom-button]")
       .click()
       .should(() => {
         expect(JSON.parse(localStorage.getItem("resumes"))).to.be.a("array");
