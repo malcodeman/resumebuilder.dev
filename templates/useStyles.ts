8a2bf92317f3b6c469933e8c00873fc900f966f0
malcodeman <26983729+malcodeman@users.noreply.github.com>
2022-12-08 21:43:27 +0100

    Improves use styles hook

--- templates/useStyles.ts
+++ templates/useStyles.ts
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { or } from "ramda";
 
 import utils from "../lib/utils";
 import theme from "./theme";
@@ -28,39 +27,71 @@ function useStyles(props: Style) {
     fontSize,
     letterSpacing,
     borderBottomWidth,
+    ...rest
   } = props;
   const { isPdf, spacing } = useContext(TemplateContext);
   const style = {
-    ...props,
-    bottom: or(isPdf ? bottom * spacing : utils.pt2px(bottom * spacing), null),
-    left: or(isPdf ? left * spacing : utils.pt2px(left * spacing), null),
-    right: or(isPdf ? right * spacing : utils.pt2px(right * spacing), null),
-    top: or(isPdf ? top * spacing : utils.pt2px(top * spacing), null),
-    backgroundColor: bgColor,
-    margin: or(isPdf ? margin * spacing : utils.pt2px(margin * spacing), null),
-    marginTop: or(isPdf ? mt * spacing : utils.pt2px(mt * spacing), null),
-    marginRight: or(isPdf ? mr * spacing : utils.pt2px(mr * spacing), null),
-    marginBottom: or(isPdf ? mb * spacing : utils.pt2px(mb * spacing), null),
-    marginLeft: or(isPdf ? ml * spacing : utils.pt2px(ml * spacing), null),
-    padding: or(
-      isPdf ? padding * spacing : utils.pt2px(padding * spacing),
-      null
-    ),
-    paddingTop: or(isPdf ? pt * spacing : utils.pt2px(pt * spacing), null),
-    paddingRight: or(isPdf ? pr * spacing : utils.pt2px(pr * spacing), null),
-    paddingBottom: or(isPdf ? pb * spacing : utils.pt2px(pb * spacing), null),
-    paddingLeft: or(isPdf ? pl * spacing : utils.pt2px(pl * spacing), null),
-    fontSize: or(
-      isPdf
+    ...rest,
+    ...(bottom && {
+      bottom: isPdf ? bottom * spacing : utils.pt2px(bottom * spacing),
+    }),
+    ...(left && {
+      left: isPdf ? left * spacing : utils.pt2px(left * spacing),
+    }),
+    ...(right && {
+      right: isPdf ? right * spacing : utils.pt2px(right * spacing),
+    }),
+    ...(top && {
+      top: isPdf ? top * spacing : utils.pt2px(top * spacing),
+    }),
+    ...(bgColor && {
+      backgroundColor: bgColor,
+    }),
+    ...(margin && {
+      margin: isPdf ? margin * spacing : utils.pt2px(margin * spacing),
+    }),
+    ...(mt && {
+      marginTop: isPdf ? mt * spacing : utils.pt2px(mt * spacing),
+    }),
+    ...(mr && {
+      marginRight: isPdf ? mr * spacing : utils.pt2px(mr * spacing),
+    }),
+    ...(mb && {
+      marginBottom: isPdf ? mb * spacing : utils.pt2px(mb * spacing),
+    }),
+    ...(ml && {
+      marginLeft: isPdf ? ml * spacing : utils.pt2px(ml * spacing),
+    }),
+    ...(padding && {
+      padding: isPdf ? padding * spacing : utils.pt2px(padding * spacing),
+    }),
+    ...(pt && {
+      paddingTop: isPdf ? pt * spacing : utils.pt2px(pt * spacing),
+    }),
+    ...(pr && {
+      paddingRight: isPdf ? pr * spacing : utils.pt2px(pr * spacing),
+    }),
+    ...(pb && {
+      paddingBottom: isPdf ? pb * spacing : utils.pt2px(pb * spacing),
+    }),
+    ...(pl && {
+      paddingLeft: isPdf ? pl * spacing : utils.pt2px(pl * spacing),
+    }),
+    ...(fontSize && {
+      fontSize: isPdf
         ? theme.fontSize[fontSize] * spacing
         : utils.pt2px(theme.fontSize[fontSize] * spacing),
-      null
-    ),
-    letterSpacing: or(letterSpacing * spacing, null),
-    borderBottomWidth: or(
-      isPdf ? borderBottomWidth : utils.pt2px(borderBottomWidth),
-      null
-    ),
+    }),
+    ...(letterSpacing && {
+      letterSpacing: isPdf
+        ? letterSpacing * spacing
+        : utils.pt2px(letterSpacing * spacing),
+    }),
+    ...(borderBottomWidth && {
+      borderBottomWidth: isPdf
+        ? borderBottomWidth
+        : utils.pt2px(borderBottomWidth),
+    }),
   };
   return { style, isPdf, spacing };
 }
