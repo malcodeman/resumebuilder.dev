6760cbe0fa6a530f1d78c0206e415b57adbaf0f5
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-02-25 18:44:51 +0100

    chore: removes pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
 const { i18n } = require("./next-i18next.config");
 
-module.exports = withPWA({ i18n });
+module.exports = { i18n };
