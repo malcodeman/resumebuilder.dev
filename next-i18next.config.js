eab7d9c86d38e172023fa7b8a8ccf1acc9590156
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-03-02 15:00:59 +0100

    fix next config

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  i18n: {
-    defaultLocale: "en",
-    locales: ["bs", "de", "en"],
-    localePath: path.resolve("./public/locales"),
-  },
-};
---
+++ next.config.js
@@ -0,0 +1,6 @@
+const i18n = {
+  defaultLocale: "en",
+  locales: ["bs", "de", "en"],
+};
+
+module.exports = { i18n };
