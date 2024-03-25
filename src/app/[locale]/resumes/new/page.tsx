d26c52b9aa7da72ebe7709c55874addbebf9e3b1
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-03-26 00:53:10 +0100

    adds e2e

--- src/app/[locale]/resumes/new/page.tsx
+++ src/app/[locale]/resumes/new/page.tsx
@@ -1,29 +1,25 @@
 "use client";
+import { useState, useEffect } from "react";
 import { Center, Spinner } from "@chakra-ui/react";
 import useResumes from "hooks/useResumes";
 import { useRouter } from "navigation";
-import { useEffect } from "react";
 
 function NewResumePage() {
-  const { resumes, createNew } = useResumes();
+  const { createNew, resumes } = useResumes();
   const router = useRouter();
+  const [isRedirected, setIsRedirected] = useState(false);
 
   useEffect(() => {
-    if (resumes) {
-      if (resumes.length > 0) {
-        router.push(`/resumes/${resumes[0].id}`);
-      } else {
-        const resume = createNew();
-        if (resume) {
-          router.push(`/resumes/${resume.id}`);
-        }
-      }
+    if (resumes && !isRedirected) {
+      const resume = createNew();
+      router.push(`/resumes/${resume.id}`);
+      setIsRedirected(true);
     }
-  }, [createNew, resumes, router]);
+  }, [resumes, isRedirected, createNew, router]);
 
   return (
     <Center flexDirection="column" minH="100vh" padding="4">
-      <Spinner />
+      <Spinner data-testid="spinner" />
     </Center>
   );
 }
---
+++ tests/new.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+import utils from "e2e/utils";
+
+test.describe("New resume page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/en/resumes/new");
+  });
+  test("Redirect", async ({ page, context, baseURL }) => {
+    expect(page.getByTestId("spinner")).toBeVisible();
+
+    const resume = await utils.getResume({ context });
+
+    await expect(page).toHaveURL(`${baseURL}/en/resumes/${resume.id}`);
+  });
+});
