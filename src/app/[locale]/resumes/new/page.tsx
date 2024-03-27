4a216734b6fb6f9fe68e8c770b77ed8a99334332
Amer K <26983729+malcodeman@users.noreply.github.com>
2024-03-27 09:31:51 +0100

    feat: adds new resume page (#82)

---
+++ src/app/[locale]/resumes/new/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Center, Spinner } from "@chakra-ui/react";
+import useResumes from "hooks/useResumes";
+import { useRouter } from "navigation";
+
+function NewResumePage() {
+  const { createNew, resumes } = useResumes();
+  const router = useRouter();
+  const [isRedirected, setIsRedirected] = useState(false);
+
+  useEffect(() => {
+    if (resumes && !isRedirected) {
+      const resume = createNew();
+      router.push(`/resumes/${resume.id}`);
+      setIsRedirected(true);
+    }
+  }, [resumes, isRedirected, createNew, router]);
+
+  return (
+    <Center flexDirection="column" minH="100vh" padding="4">
+      <Spinner data-testid="spinner" />
+    </Center>
+  );
+}
+
+export default NewResumePage;
