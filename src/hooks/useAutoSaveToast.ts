71d92a53f1791f73c8a50bea650c9cb31a1d037a
Amer K <26983729+malcodeman@users.noreply.github.com>
2024-05-29 19:01:37 +0200

    feat: adds hotkeys hook (#90)

--- src/hooks/useAutoSaveToast.ts
+++ src/hooks/useAutoSaveToast.ts
@@ -1,9 +1,8 @@
-import React from "react";
 import { useToast } from "@chakra-ui/toast";
 import { length } from "ramda";
-import { useMountEffect } from "@react-hookz/web";
 import { useTranslations } from "next-intl";
 import utils from "lib/utils";
+import { useHotkeys } from "react-hotkeys-hook";
 
 const TOAST_ID = "onSave";
 const SAVE_MESSAGE_TRANS_KEYS = [
@@ -27,37 +26,22 @@ function useAutoSaveToast(props: props) {
     isClosable = true,
   } = props;
   const t = useTranslations();
-  const [keyboardJs, setKeyboardJs] = React.useState(null);
   const toast = useToast();
 
-  useMountEffect(() => {
-    import("keyboardjs").then((k) => setKeyboardJs(k.default || k));
+  function handleOnSave() {
+    if (!toast.isActive(TOAST_ID)) {
+      toast({
+        id: TOAST_ID,
+        description: t(description),
+        duration,
+        isClosable,
+      });
+    }
+  }
+
+  useHotkeys(["meta+s", "ctrl+s"], handleOnSave, {
+    preventDefault: true,
   });
-
-  React.useEffect(() => {
-    if (!keyboardJs) {
-      return;
-    }
-
-    function handleOnSave(event: React.KeyboardEvent) {
-      event.preventDefault();
-      if (!toast.isActive(TOAST_ID)) {
-        toast({
-          id: TOAST_ID,
-          description: t(description),
-          duration,
-          isClosable,
-        });
-      }
-    }
-
-    keyboardJs.bind(`ctrl + s`, handleOnSave);
-    keyboardJs.bind(`command + s`, handleOnSave);
-    return () => {
-      keyboardJs.unbind(`ctrl + s`, handleOnSave);
-      keyboardJs.unbind(`command + s`, handleOnSave);
-    };
-  }, [keyboardJs, toast, description, duration, isClosable, t]);
 }
 
 export default useAutoSaveToast;
