81f7b236113da76542901d7e0f9605a82d96929e
Amer K <26983729+malcodeman@users.noreply.github.com>
2024-05-30 16:43:26 +0200

    feat: removes cypress (#92)

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    video: false,
-    screenshotOnRunFailure: false,
-    baseUrl: "http://localhost:3000",
-  },
-});
