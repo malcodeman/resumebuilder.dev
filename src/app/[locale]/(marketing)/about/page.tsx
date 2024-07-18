d6bea0ebf1e700d5f3a38fee2421938c135e138a
Amer K <26983729+malcodeman@users.noreply.github.com>
2024-07-18 06:33:43 +0200

    feat: adds about page metadata (#122)

--- src/app/[locale]/(marketing)/about/page.tsx
+++ src/app/[locale]/(marketing)/about/page.tsx
@@ -1,32 +1,12 @@
-"use client";
-import { Text, Heading, Container, Avatar, Box } from "@chakra-ui/react";
-import { useTranslations } from "next-intl";
-import { Footer } from "app/[locale]/(marketing)/_components/Footer";
-import { Stats } from "app/[locale]/(marketing)/_components/Stats";
+import { Metadata } from "next";
+import { About } from "app/[locale]/(marketing)/_components/About";
 
-function About() {
-  const t = useTranslations();
+export const metadata: Metadata = {
+  title: "About | resumebuilder.dev",
+};
 
-  return (
-    <>
-      <Container maxW="container.sm">
-        <Heading mb="4">{t("about")}</Heading>
-        <Text mb="4">{t("about_description_1")}</Text>
-        <Text mb="4">{t("about_description_2")}</Text>
-        <Text mb="4">{t("about_description_3")}</Text>
-        <Heading mb="4">{t("complete_platform_for_career")}</Heading>
-        <Text mb="4">{t("about_description_4")}</Text>
-        <Text mb="4">{t("about_description_5")}</Text>
-        <Text mb="4">{t("about_description_6")}</Text>
-        <Stats mb="4" />
-        <Box mb="32">
-          <Avatar name="Amer Karamustafić" src="/about/avatar.png" />
-          <Text>Amer Karamustafić, {t("founder_of_resumebuilder")}</Text>
-        </Box>
-      </Container>
-      <Footer />
-    </>
-  );
+function AboutPage() {
+  return <About />;
 }
 
-export default About;
+export default AboutPage;
